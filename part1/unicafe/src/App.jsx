747375cbd131491d3d8ea5ae3c87464cc5b15a35
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-09-19 11:22:52 +0300

    Step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -50,19 +50,26 @@ const Statistics = ({statistics}) => {
   return (
     <div className="statistics">
       <h2>statistics</h2>
-      <StatisticLine text="good" value={statistics.good} />
-      <StatisticLine text="neutral" value={statistics.neutral} />
-      <StatisticLine text="bad" value={statistics.bad} />
-      <StatisticLine text="all" value={sum} />
-      <StatisticLine text="average" value={(statistics.good - statistics.bad) / sum} />
-      <StatisticLine text="positive" value={statistics.good / sum * 100} unit="%" />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={statistics.good} />
+          <StatisticLine text="neutral" value={statistics.neutral} />
+          <StatisticLine text="bad" value={statistics.bad} />
+          <StatisticLine text="all" value={sum} />
+          <StatisticLine text="average" value={(statistics.good - statistics.bad) / sum} />
+          <StatisticLine text="positive" value={statistics.good / sum * 100} unit="%" />
+        </tbody>
+      </table>
     </div>
   )
 }
 
 const StatisticLine = ({text, value, unit=""}) => {
   return (
-    <p>{text} {value}{unit && ' ' + unit}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}{unit && ' ' + unit}</td> 
+    </tr>
   )
 }
 
