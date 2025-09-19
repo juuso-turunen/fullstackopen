9330111a1990c433873346907fd189f61b2c553b
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-09-19 10:38:53 +0300

    Step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,48 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const onGood = () => {
+    setGood(good + 1)
+  }
+
+  const onNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const onBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={onGood} text="good" />
+      <Button onClick={onNeutral} text="neutral" />
+      <Button onClick={onBad} text="bad" />
+
+      <Statistics statistics={{} = {good, neutral, bad}}/>
+    </div>
+
+  )
+}
+
+const Button = ({text, onClick}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Statistics = ({statistics}) => {
+  return (
+    <div className="statistics">
+      <h2>statistics</h2>
+      <p>good {statistics.good}</p>
+      <p>neutral {statistics.neutral}</p>
+      <p>bad {statistics.bad}</p>
     </div>
   )
 }
