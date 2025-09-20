691cb1f2f5fe91b7dfd12ea6f61001e825fa5677
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-09-20 18:24:01 +0300

    Step 4

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -2,10 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  const [filteredPersons, setFilteredPersons] = useState(persons)
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const filterPersons = (e) => {
+    const newSearchQuery = e.target.value
+    setSearchQuery(newSearchQuery)
+    setFilteredPersons(persons.filter((person) => person.name.toLocaleLowerCase().includes(newSearchQuery.toLocaleLowerCase())))
+  }
   
   const addNewPerson = (e) => {
     e.preventDefault()
@@ -25,6 +38,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchQuery} onChange={filterPersons} />
+      </div>
+
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
@@ -36,7 +54,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) =>
+        {filteredPersons.map((person) =>
           <li key={person.name}>{person.name}: {person.number}</li>
         )}
       </ul>
