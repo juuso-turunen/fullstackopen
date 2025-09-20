ff34cffe7d423ebcaba7f15fabcd091557e110f8
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-09-20 17:41:53 +0300

    Step 1

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -6,19 +6,37 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const addNewName = (e) => {
+    e.preventDefault()
+
+    const newPersons = [...persons, {name: newName}]
+    setPersons(newPersons)
+
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={addNewName} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) =>
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
+      <div>debug: {newName}</div>
     </div>
   )
 
