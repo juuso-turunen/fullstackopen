2a2de90fe59a3c4560d98fe698b1e4656504a3c2
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-09-27 16:41:34 +0300

    Step 8

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -3,7 +3,16 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", function (req, res) {
+    return JSON.stringify(req.body);
+});
+
+app.use(
+    morgan(
+        ":method :url :status :res[content-length] - :response-time ms :body"
+    )
+);
 
 let persons = [
     {
@@ -89,4 +98,3 @@ const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
