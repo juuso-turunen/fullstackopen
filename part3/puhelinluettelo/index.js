063876caf4bf329f0b1a7fccac4a5d61b2f87748
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-09-27 15:38:31 +0300

    Step 3

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -3,22 +3,22 @@ const app = express();
 
 let persons = [
     {
-        id: 1,
+        id: "1",
         name: "Arto Hellas",
         number: "040-123456",
     },
     {
-        id: 2,
+        id: "2",
         name: "Ada Lovelace",
         number: "39-44-5323523",
     },
     {
-        id: 3,
+        id: "3",
         name: "Dan Abramov",
         number: "12-43-234345",
     },
     {
-        id: 4,
+        id: "4",
         name: "Mary Poppendieck",
         number: "39-23-6423122",
     },
@@ -26,6 +26,18 @@ let persons = [
 
 app.get("/api/persons", (request, response) => {
     response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = request.params.id;
+
+    const person = persons.find((person) => person.id === id);
+
+    if (person) {
+        return response.json(person);
+    }
+
+    return response.status(404).end();
 });
 
 app.get("/info", (request, response) => {
