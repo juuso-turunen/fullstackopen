63357c34d80b7388ed123bb7fb7fcdf183457ed8
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-09-28 14:14:42 +0300

    Removed cors package

--- part3/puhelinluettelo-backend/index.js
+++ part3/puhelinluettelo-backend/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const cors = require("cors");
-
-app.use(cors());
 
 app.use(express.static("dist"));
 
