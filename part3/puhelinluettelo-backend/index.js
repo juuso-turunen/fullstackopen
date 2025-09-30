71e19dfe6793d18c2e99f9ac0c81e7ce29e50557
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-09-30 12:00:18 +0300

    added unknown endpoint handler

--- part3/puhelinluettelo-backend/index.js
+++ part3/puhelinluettelo-backend/index.js
@@ -96,6 +96,12 @@ app.get("/info", (request, response) => {
         `);
 });
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
