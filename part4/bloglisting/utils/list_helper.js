dbc4ebf6048432a74b72a58ebeda75fa59cc8641
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2025-10-01 15:36:06 +0300

    e 4.4

--- part4/bloglisting/utils/list_helper.js
+++ part4/bloglisting/utils/list_helper.js
@@ -1,7 +1,16 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sum = blogs.reduce((acc, blog) => {
+    return acc + blog.likes
+  }, 0)
+
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
