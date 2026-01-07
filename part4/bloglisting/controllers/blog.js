300a7eab2e62404d47c348a32f167b1911fc10b4
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2026-01-07 15:31:45 +0200

    e 4.19

--- part4/bloglisting/controllers/blog.js
+++ part4/bloglisting/controllers/blog.js
@@ -1,15 +1,31 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogRouter.post('/', async (request, response) => {
-  const users = await User.find({})
-  const currentUser = users[0]
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.JWT_SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const currentUser = await User.findById(decodedToken.id)
+
+  if (!currentUser) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
 
   const blog = new Blog({ ...request.body, user: currentUser })
 
