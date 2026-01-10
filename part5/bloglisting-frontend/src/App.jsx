fc1416bebdacb387af3f9d17365d511c485b0bbd
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2026-01-10 17:43:37 +0200

    e 5.2

--- part5/bloglisting-frontend/src/App.jsx
+++ part5/bloglisting-frontend/src/App.jsx
@@ -14,10 +14,19 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedInUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedInUser", JSON.stringify(user));
       setUser(user);
       setUsername("");
       setPassword("");
@@ -27,6 +36,11 @@ const App = () => {
         setErrorMessage(null);
       }, 5000);
     }
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedInUser");
+    setUser(null);
   };
 
   if (user === null) {
@@ -62,7 +76,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
