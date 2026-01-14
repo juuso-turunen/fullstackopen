18d681985b81830b65bfc97612d7d83643d053ea
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2026-01-14 11:45:59 +0200

    e 5.10

--- part5/bloglisting-frontend/src/App.jsx
+++ part5/bloglisting-frontend/src/App.jsx
@@ -108,7 +108,7 @@ const App = () => {
         <BlogForm handleSubmit={handleCreateBlog} />
       </Togglable>
 
-      {blogs.map((blog) => (
+      {blogs.sort((a, b) => b.likes - a.likes).map((blog) => (
         <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
       ))}
     </div>
