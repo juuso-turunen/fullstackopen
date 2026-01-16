424b3da07816b08470538880fcef7fc9fd8fca7a
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2026-01-16 15:11:25 +0200

    e 5.14

--- part5/bloglisting-frontend/src/components/Blogs.test.jsx
+++ part5/bloglisting-frontend/src/components/Blogs.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { test } from 'vitest'
 
 describe('<Togglable />', () => {
   test('renders content', () => {
@@ -31,5 +33,32 @@ describe('<Togglable />', () => {
 
     expect(author).toBeNull()
     expect(url).toBeNull()
+  })
+
+  test('renders url, likes and author after full details button is pressed', async () => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      url: 'https://example.com',
+      author: 'Test Author',
+      likes: 4,
+      user: {
+        name: 'Test User',
+        username: 'testuser'
+      }
+    }
+
+    render(<Blog blog={blog} user={{ username: 'testuser' }} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url = screen.getByText(blog.url)
+    const userName = screen.getByText(blog.user.name)
+    const likes = screen.getByText(`likes ${blog.likes}`)
+
+    expect(url).toBeDefined()
+    expect(userName).toBeDefined()
+    expect(likes).toBeDefined()
   })
 })
