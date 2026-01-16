7a2c13ddcea8bc60e54bb80df1caa8c77873ff04
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2026-01-16 15:35:01 +0200

    e 5.15

--- part5/bloglisting-frontend/src/components/Blogs.test.jsx
+++ part5/bloglisting-frontend/src/components/Blogs.test.jsx
@@ -3,6 +3,12 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 import { test } from 'vitest'
 
+vi.mock('../services/blogs', () => ({
+  default: {
+    put: vi.fn().mockResolvedValue({})
+  }
+}))
+
 describe('<Togglable />', () => {
   test('renders content', () => {
     const blog = {
@@ -60,5 +66,32 @@ describe('<Togglable />', () => {
     expect(url).toBeDefined()
     expect(userName).toBeDefined()
     expect(likes).toBeDefined()
+  })
+
+  test('if liked twice the event handler is called twice', async () => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      url: 'https://example.com',
+      author: 'Test Author',
+      likes: 4,
+      user: {
+        name: 'Test User',
+        username: 'testuser'
+      }
+    }
+
+    const mockHandler = vi.fn()
+
+    render(<Blog blog={blog} user={{ username: 'testuser' }} updateBlog={mockHandler} />)
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
