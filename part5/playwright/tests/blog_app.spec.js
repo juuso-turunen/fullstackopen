21d8bc8726f8b037b479dabb13ab297e92f913b9
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2026-01-21 13:13:44 +0200

    e 5.18

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,21 +1,46 @@
-const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { test, expect, beforeEach, describe } = require("@playwright/test");
 
-describe('Blog app', () => {
+describe("Blog app", () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('http://localhost:3003/api/testing/reset')
-    await request.post('http://localhost:3003/api/users', {
+    await request.post("http://localhost:3003/api/testing/reset");
+    await request.post("http://localhost:3003/api/users", {
       data: {
-        name: 'Matti Luukkainen',
-        username: 'mluukkai',
-        password: 'salainen'
-      }
-    })
+        name: "Juuso Turunen",
+        username: "juuso",
+        password: "juusonsalasana",
+      },
+    });
 
-    await page.goto('http://localhost:5173')
-  })
+    await page.goto("http://localhost:5173");
+  });
+  
+  test("Login form is shown", async ({ page }) => {
+    await expect(page.getByText("log in to application")).toBeVisible();
+    await expect(page.getByRole("button", { name: "login" })).toBeVisible();
+  });
+  
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.goto("http://localhost:5173");
 
-  test('Login form is shown', async ({ page }) => {
-    await expect(page.getByText('log in to application')).toBeVisible()
-    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
-  })
-})
+      await page.getByLabel('username').fill('juuso')
+      await page.getByLabel('password').fill('juusonsalasana')
+
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Juuso Turunen logged in')).toBeVisible()
+    });
+    
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.goto("http://localhost:5173");
+  
+      await page.getByLabel('username').fill('juuso')
+      await page.getByLabel('password').fill('pekansalasana')
+  
+      await page.getByRole('button', { name: 'login' }).click()
+  
+      await expect(page.getByText('Juuso Turunen logged in')).toBeHidden()
+      await expect(page.getByText('wrong credentials')).toBeVisible()
+    });
+  });
+});
