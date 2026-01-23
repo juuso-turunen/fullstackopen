49d9b9f4b1452bdabfbcdcee1ad2de7cef62f507
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2026-01-23 15:45:52 +0200

    e 6.1

--- part6/unicafe-redux/src/reducers/counterReducer.js
+++ part6/unicafe-redux/src/reducers/counterReducer.js
@@ -8,11 +8,20 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
+      return {
+        ...state,
+        good: state.good + 1
+      }
+      case 'OK':
+      return {
+        ...state,
+        ok: state.ok + 1
+      }
+      case 'BAD':
+      return {
+        ...state,
+        bad: state.bad + 1
+      }
     case 'RESET':
       return state
     default:
